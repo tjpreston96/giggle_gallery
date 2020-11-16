5ac633c5160b0e56ba2b72368c27f03a13b0e2e3
tpreston96 <tpreston96@gmail.com>
2020-11-16 10:51:21 -0600

    fixed errors and added colorScheme

--- js/app.js
+++ js/app.js
@@ -1,18 +1,23 @@
 console.log(`sanity check`);
 
 /*-------------------------------- Constants --------------------------------*/
+const colorScheme = {
+    dark: '',
+    change: function () {
+        colorScheme.dark = colorScheme.dark ? '' : 'dark';
+        body.setAttribute('class', colorScheme.dark)
+    },
+}
+/*---------------------------- Variables (state) ----------------------------*/
 
+const jokes
+
+/*------------------------ Cached Element References ------------------------*/
 const norrisBtn
 const genBtn
 const container
 const lightDarkBrn
 const body
-/*---------------------------- Variables (state) ----------------------------*/
-
-
-
-/*------------------------ Cached Element References ------------------------*/
-
 
 
 /*----------------------------- Event Listeners -----------------------------*/
