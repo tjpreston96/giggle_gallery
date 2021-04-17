25175376dc44195f12074365281afcfbf9782a29
tpreston96 <tpreston96@gmail.com>
2021-04-17 18:43:22 -0500

    card sizing

--- js/app.js
+++ js/app.js
@@ -74,7 +74,7 @@ progBtn.addEventListener("click", () => {
 function appendDiv(joke, category, idx) {
   let newDiv = document.createElement("div");
   newDiv.innerHTML = `
-                        <div class="card h-100" id="${category.toLowerCase()}">
+                        <div class="card" id="${category.toLowerCase()}">
                           <div class='card-header ${category}-header'><b>${category}</b></div>
                           <div class="card-body">
                             <p>${joke}</p>                              
@@ -100,7 +100,7 @@ function appendDiv2(category, setup, punchline, idx) {
   let newDiv = document.createElement("div");
 
   newDiv.innerHTML = `
-                        <div class="card h-100" id="${category.toLowerCase()}">
+                        <div class="card" id="${category.toLowerCase()}">
                         <div class='card-header ${category}-header'><b>${
     category[0].toUpperCase() + category.slice(1)
   }</b></div>
